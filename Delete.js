aba639a98939e00768c298a1ca8ad2ebab49af2d
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2025-05-08 22:12:00 +0300

    remove Stripe dependency and update integration page structure

--- Delete.js
+++
@@ -1,7 +0,0 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
-// Wrap in an async function since top-level await isn't allowed in CommonJS
-(async () => {
-  const deleted = await stripe.accounts.del("acct_1RLV26Ikl1Zk74oE");
-  console.log("Deleted account:", deleted);
-})();
--- src/app/dashboard/integration/page.tsx
+++ src/app/dashboard/integration/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import ConnectStripeButton from "@/components/pages/Dashboard/Integration/Integration";
 
 const integrationPage = () => {
   return (
     <>
-      <ConnectStripeButton />
+      <div>integrationPage</div>
     </>
   );
 };
