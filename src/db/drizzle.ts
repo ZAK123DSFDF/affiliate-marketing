7765346761d056854b2733a39d58f04057b89a13
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2025-05-22 18:39:13 +0300

    refactor database connection setup to simplify initialization

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,6 +1,2 @@
-import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
-import { config } from "dotenv";
-config({ path: ".env" });
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql });
+const db = drizzle(process.env.DATABASE_URL!);
