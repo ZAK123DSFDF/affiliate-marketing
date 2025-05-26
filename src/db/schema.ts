efcc4c8fe31dfe8bd0ca3d918b4c55fd29482336
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2025-05-26 15:54:42 +0300

    feat: define user and session tables in schema for user management

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,23 +1,36 @@
 import {
   pgTable,
-  uuid,
-  varchar,
-  integer,
-  pgEnum,
   text,
+  timestamp,
+  boolean,
+  integer,
 } from "drizzle-orm/pg-core";
 
-// Define enum for payment providers
-export const paymentProviderEnum = pgEnum("payment_provider", [
-  "stripe",
-  "paddle",
-  "lemon_squeezy",
-]);
+export const user = pgTable("user", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  emailVerified: boolean("email_verified")
+    .$defaultFn(() => false)
+    .notNull(),
+  image: text("image"),
+  createdAt: timestamp("created_at")
+    .$defaultFn(() => /* @__PURE__ */ new Date())
+    .notNull(),
+  updatedAt: timestamp("updated_at")
+    .$defaultFn(() => /* @__PURE__ */ new Date())
+    .notNull(),
+});
 
-export const users = pgTable("users", {
-  id: uuid().primaryKey().defaultRandom(),
-  name: text().notNull(),
-  age: integer().notNull(),
-  email: text().notNull().unique(),
-  paymentProvider: paymentProviderEnum().notNull(),
+export const session = pgTable("session", {
+  id: text("id").primaryKey(),
+  expiresAt: timestamp("expires_at").notNull(),
+  token: text("token").notNull().unique(),
+  createdAt: timestamp("created_at").notNull(),
+  updatedAt: timestamp("updated_at").notNull(),
+  ipAddress: text("ip_address"),
+  userAgent: text("user_agent"),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
 });
