1148e0f21dfacf04e8205e840474d4ab73dd177e
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2025-05-26 17:34:38 +0300

    feat: integrate organization and authentication plugins, and define new database tables for account, verification, organization, member, and invitation

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,9 @@
 import { createAuthClient } from "better-auth/react";
+import { organizationClient } from "better-auth/client/plugins";
 const isProduction = process.env.NODE_ENV === "production";
 export const authClient = createAuthClient({
   baseURL: isProduction
     ? "https://affiliate-marketing-ten.vercel.app"
     : "http://myapp.test:3000",
+  plugins: [organizationClient()],
 });
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,11 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db/drizzle";
-
+import { openAPI, organization } from "better-auth/plugins";
+import { nextCookies } from "better-auth/next-js";
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+  plugins: [organization(), nextCookies(), openAPI()],
 });
