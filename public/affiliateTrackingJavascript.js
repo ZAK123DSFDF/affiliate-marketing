698ba7beb07e859340e14d8ba5f36ac1fc377b5e
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2025-07-09 10:25:32 +0300

    edit

--- public/affiliateTrackingJavascript.js
+++ public/affiliateTrackingJavascript.js
@@ -1476,7 +1476,7 @@
        }
        async function storeRefCode(code) {
            try {
-               const res = await fetch(`${ORGID_ENDPOINT}?code=${encodeURIComponent(code)}`, { credentials: "include" });
+               const res = await fetch(`${ORGID_ENDPOINT}?code=${encodeURIComponent(code)}`);
                if (!res.ok)
                    throw new Error("Failed to fetch organization info");
                const { cookieLifetimeValue, cookieLifetimeUnit, commissionType, commissionValue, commissionDurationValue, commissionDurationUnit, } = await res.json();
@@ -1529,7 +1529,6 @@
                    headers: { "Content-Type": "application/json" },
                    body: payload,
                    keepalive: true,
-                   credentials: "include",
                }).catch(() => { });
            }
        }
