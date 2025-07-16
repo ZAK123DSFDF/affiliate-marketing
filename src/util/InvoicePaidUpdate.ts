680bcd02de523bb6713546005145e120203b0eb8
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2025-07-16 15:42:42 +0300

    removed db transaction on the web hook

--- src/util/InvoicePaidUpdate.ts
+++ src/util/InvoicePaidUpdate.ts
@@ -3,13 +3,9 @@ import { safeFormatAmount } from "@/util/SafeParse";
 import { getCurrencyDecimals } from "@/util/CurrencyDecimal";
 import { convertToUSD } from "@/util/CurrencyConvert";
 import { affiliateInvoice } from "@/db/schema";
-import { NeonHttpDatabase } from "drizzle-orm/neon-http";
-import { PgTransaction } from "drizzle-orm/pg-core";
-type DBorTx =
-  | NeonHttpDatabase<typeof import("@/db/schema")>
-  | PgTransaction<any, any, any>;
+import { db } from "@/db/drizzle";
+
 export const invoicePaidUpdate = async (
-  tx: DBorTx,
   total: string,
   currency: string,
   customerId: string,
@@ -34,7 +30,7 @@ export const invoicePaidUpdate = async (
     addedCommission =
       parseFloat(invoiceAmount) < 0 ? 0 : parseFloat(commissionValue);
   }
-  await tx.insert(affiliateInvoice).values({
+  await db.insert(affiliateInvoice).values({
     paymentProvider: "stripe",
     subscriptionId,
     customerId,
