cb5d38b5bd782284d84ba84b7900b53a8c413832
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2025-07-25 11:32:44 +0300

    refactor Forgot-password component to use Mail icon directly instead of JSX element

--- src/components/pages/Forgot-password.tsx
+++ src/components/pages/Forgot-password.tsx
@@ -77,7 +77,7 @@ const ForgotPassword = () => {
                   label="Email"
                   placeholder="john.doe@example.com"
                   type="email"
-                  icon={<Mail className="h-5 w-5 text-muted-foreground" />}
+                  icon={Mail}
                 />
 
                 <Button type="submit" className="w-full" disabled={pending}>
