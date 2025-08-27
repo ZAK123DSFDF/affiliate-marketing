a44906c6726d4ebdccdf6b113aeb1561e19f0a47
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2025-08-27 22:17:55 +0300

    feat(seeds): add script to generate database seed files and update seeding logic

--- src/db/seeds/seedFun.ts
+++ src/db/seeds/seedFun.ts
@@ -1,3 +1,16 @@
+const {
+  affiliateClick_seed,
+  affiliateInvoice_seed,
+  affiliateLink_seed,
+  affiliate_seed,
+  organizationAuthCustomization_seed,
+  organizationDashboardCustomization_seed,
+  organization_seed,
+  userOrganization_seed,
+  user_seed,
+  //@ts-ignore
+} = await import("@/db/seeds/databaseObjects")
+
 import { db } from "@/db/drizzle"
 import {
   user,
@@ -10,40 +23,29 @@ import {
   organizationAuthCustomization,
   organizationDashboardCustomization,
 } from "@/db/schema"
-import {
-  affiliate_click_seed,
-  affiliate_invoice_seed,
-  affiliate_link_seed,
-  affiliate_seed,
-  organization_auth_customization_seed,
-  organization_dashboard_customization_seed,
-  organization_seed,
-  user_organization_seed,
-  user_seed,
-} from "@/db/seeds/databaseSeed"
 
 async function seedFun() {
   await db.insert(organization).values(organization_seed)
   await db.insert(user).values(user_seed)
-  await db.insert(userToOrganization).values(user_organization_seed)
+  await db.insert(userToOrganization).values(userOrganization_seed)
   await db.insert(affiliate).values(affiliate_seed)
-  await db.insert(affiliateLink).values(affiliate_link_seed)
-  await db.insert(affiliateClick).values(affiliate_click_seed)
-  await db.insert(affiliateInvoice).values(affiliate_invoice_seed)
+  await db.insert(affiliateLink).values(affiliateLink_seed)
+  await db.insert(affiliateClick).values(affiliateClick_seed)
+  await db.insert(affiliateInvoice).values(affiliateInvoice_seed)
   await db
     .insert(organizationAuthCustomization)
-    .values(organization_auth_customization_seed)
+    .values(organizationAuthCustomization_seed)
   await db
     .insert(organizationDashboardCustomization)
-    .values(organization_dashboard_customization_seed)
+    .values(organizationDashboardCustomization_seed)
 }
 
 seedFun()
   .then(() => {
-    console.log("✅ Seed completed")
+    console.log("✅ Auto-seed completed")
     process.exit(0)
   })
   .catch((err) => {
-    console.error("❌ Seed failed", err)
+    console.error("❌ Auto-seed failed", err)
     process.exit(1)
   })
