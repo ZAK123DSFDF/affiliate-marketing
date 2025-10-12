17bfc7a225f5e9e51951192bb560305d8e911241
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2025-10-12 13:07:33 +0300

    feat: add authentication check for affiliates in InvalidTokenPage component

--- src/app/affiliate/[orgId]/(auth)/invalid-token/page.tsx
+++ src/app/affiliate/[orgId]/(auth)/invalid-token/page.tsx
@@ -1,9 +1,11 @@
 import InvalidToken from "@/components/pages/InvalidToken"
 import { getValidatedOrgFromParams } from "@/util/getValidatedOrgFromParams"
 import { OrgIdProps } from "@/lib/types/orgId"
+import { redirectIfAffiliateAuthed } from "@/lib/server/authGuards"
 
 const InvalidTokenPage = async ({ params }: OrgIdProps) => {
   const orgId = await getValidatedOrgFromParams({ params })
+  await redirectIfAffiliateAuthed(orgId)
   return <InvalidToken orgId={orgId} affiliate />
 }
 
